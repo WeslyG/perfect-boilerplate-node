2135911cac83f74dcf490c71f0b9dc02e5908aca
WeslyG <weslyg22@gmail.com>
2020-08-18 23:40:42 +0500

    update node types and disable cors fix

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 
 export const corsDisable = (app: Express): void => {
   console.log('App run in DEV mode, disable cors');
-  app.use((req, res, next) => {
+  app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     next();
